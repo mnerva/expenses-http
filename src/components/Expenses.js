8dc3139ee4d82c63a601cf7b1809600c1cfe6ba9
Mona <mona-rea.nerva@voco.ee>
2023-10-21 19:30:39 +0300

    juhend 1-5

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,2 +1,14 @@
 import './Expenses.css'
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
+function Expenses(props){
+	return (
+		<Card className="expenses">
+			<ExpenseItem expenseData={props.expenses[0]}></ExpenseItem>
+			<ExpenseItem expenseData={props.expenses[1]}></ExpenseItem>
+		</Card>
+		)
+}
+
+export default Expenses
